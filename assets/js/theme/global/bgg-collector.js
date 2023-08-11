e315596ffa270886561b116743d409f84477e935
David González Polán <dpolan@oldboycd.com>
2023-08-11 09:03:58 +0200

    Store BGG username and redirect to the collector page

---
+++ assets/js/theme/global/bgg-collector.js
@@ -0,0 +1,22 @@
+export default function () {
+  const form = document.querySelector('[data-form="collector-widget"]');
+
+  if (form !== null) {
+    // Find form submission to catch BGG user and store it on the application storage
+    form.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+
+      const input = form.querySelector("input");
+      const username = input.value;
+
+      if (username.length > 0) {
+        // Store it
+        localStorage.setItem("bgg-user", username);
+
+        // Redirect to custom page template
+        // TODO: Make it dynamic
+        window.location = "http://localhost:3000/bgg-collector/";
+      }
+    });
+  }
+}
